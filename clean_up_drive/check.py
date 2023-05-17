7760a78a1ebe0f4db697cd63067e8ec8a9f6b3b1
Elaija Heart Velasco <21-01226@g.batstate-u.edu.ph>
2023-05-17 21:27:58 +0800

    Add files via upload

---
+++ clean_up_drive/check.py
@@ -0,0 +1,11 @@
+from tkinter import *
+import tkinter as ttk
+from PIL import ImageTk
+
+info_window=Tk()
+info_window.title('Done')
+info_window.iconbitmap(r'ocean.ico')
+info_window.geometry('850x720')
+info_window.config(bg='gray90')
+
+info_window.mainloop()
